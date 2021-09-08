a9c6da111aa9ae9b9de4105837a4c4ec9338731b
AliReza <AliRezaDavodiofficial@yahoo.com>
2021-09-08 15:28:50 +0430

    added addEventListener to moveUp and moveDown labels

--- frontend-mentorship-challenge-02/js/script.js
+++ frontend-mentorship-challenge-02/js/script.js
@@ -1,27 +1,53 @@
+// var 
 const inputBox = document.querySelectorAll('.float');
 const inputBoxArr = Array.from(inputBox);
 const createAccountBtn = document.querySelector('.btn')
 const checkBox = document.getElementById('accept')
-inputBoxArr.map(ele => ele.addEventListener('keyup', validate))
 
 
+// event listener
+inputBoxArr.map(ele => ele.addEventListener('keyup', validate));
+inputBoxArr.map(ele => ele.children[0].addEventListener('focus', moveReplaceUp))
+inputBoxArr.map(ele => ele.children[0].addEventListener('blur', moveReplaceDown))
 
-function validate(elem) {
-
-    // check value or it's length
-    if (elem.target.value && elem.target.value.length >= 10) {
-        elem.target.classList.add('valid')
-        elem.target.classList.remove('unvalid')
-    }
-    else {
-        elem.target.classList.add('unvalid')
-        elem.target.classList.remove('valid')
-    }
-
-}
-
+createAccountBtn.addEventListener('click', () => {
+    inputBoxArr.map(elem => {
+        const input = elem.children[0]
+        if (!elem.value) {
+            input.classList.add('unvalid')
+            input.classList.remove('valid')
+        }
+    })
+});
 
 checkBox.addEventListener('click', () => {
     createAccountBtn.classList.toggle('not-allow')
     createAccountBtn.classList.toggle('active')
+    createAccountBtn.toggleAttribute('disabled')
 })
+
+
+
+//functions
+function validate(event) {
+
+    // check value or it's length
+    if (event.target.value && event.target.value.length >= 10) {
+        event.target.classList.add('valid')
+        event.target.classList.remove('unvalid')
+    }
+    else {
+        event.target.classList.add('unvalid')
+        event.target.classList.remove('valid')
+    }
+}
+
+function moveReplaceUp(event) {
+        event.target.parentElement.children[1].classList.add('float-label-active')
+}
+
+function moveReplaceDown(event) {
+    if (!event.target.value) {
+        event.target.parentElement.children[1].classList.remove('float-label-active')
+    }
+}
